2e723619cdda8b3fb5e5095e8888d97555ee130f
lehdao2072000 <lethanhdao207@gmail.com>
2022-05-09 09:33:04 +0700

    update search v1

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,26 +1,62 @@
 import React from "react";
 import { useState } from "react";
-function TodoList() {
+
+const TodoList = () => {
+  // dataTitles , setDataTitles
   const [titles, setTitles] = useState([]);
   const [title, setTitle] = useState("");
-  const [todotitleUpdate, settodoTitleUpdate] = useState(null);
+  const [todoTitleUpdate, setTodoTitleUpdate] = useState(null);
+  // nameUpdate
   const [titleUpdate, setTitleUpdate] = useState("");
-  ///
-  function AddTitle() {
+  // name search
+  const [titleSearch, setTitleSearch] = useState("");
+
+  const DELETE = "DELETE";
+  const ADD = "ADD";
+  const CONFIG_CRUD = {
+    DELETE: DELETE,
+    ADD: ADD,
+  };
+
+  const addTitle = () => {
     const newtitles = {
       id: Math.floor(Math.random() * 1000),
       text: title,
     };
     setTitles([...titles].concat(newtitles));
     setTitle("");
-  }
-  //
-  function DeleteTitle(id) {
+  };
+  //chưa làm được
+  /*
+  const caseFnCrud = (caseFn) => {
+    const caseName = caseFn;
+    switch (caseName) {
+      case caseName === CONFIG_CRUD.ADD:
+        const newTitle = {
+          id: Math.floor(Math.random() * 1000),
+          Text: title,
+        };
+        console.log(newTitle);
+        setTitles([...titles].concat(newTitle));
+        setTitle("");
+        break;
+      case CONFIG_CRUD.DELETE === caseFn:
+        // xử lý code
+        //AddTitle();
+
+        break;
+      default:
+        break;
+    }
+  };*/
+  // caseFnCrud(DELETE);
+
+  const deleteTitle = (id) => {
     const update = [...titles].filter((title) => title.id !== id);
     setTitles(update);
-  }
+  };
 
-  function UpdateTitle(id) {
+  const updateTitle = (id) => {
     const update = [...titles].map((title) => {
       if (title.id === id) {
         title.text = titleUpdate;
@@ -28,22 +64,76 @@ function TodoList() {
       return title;
     });
     setTitles(update);
-    settodoTitleUpdate(null);
-    setTitleUpdate("");
-  }
+    setTodoTitleUpdate(null);
+    //setTitleUpdate("");
+  };
 
+  const searchTitle = () => {
+    const update = [...titles].filter((title, index) => {
+      console.log(
+        title.text.toLowerCase().includes(titleSearch.toLocaleLowerCase())
+      );
+      if (title.text.toLowerCase().includes(titleSearch.toLocaleLowerCase())) {
+        return (
+          <li key={title.id}>
+            <span>{index + 1}</span>
+
+            {todoTitleUpdate === title.id ? (
+              <div>
+                <input
+                  type="text"
+                  name="updateTextTodo"
+                  value={titleUpdate}
+                  onChange={(e) => setTitleUpdate(e.target.value)}
+                />
+                <button onClick={() => updateTitle(title.id)}>Done</button>
+              </div>
+            ) : (
+              <p>{title.text}</p>
+            )}
+
+            <div>
+              <button
+                onClick={() => {
+                  setTodoTitleUpdate(title.id);
+                  setTitleUpdate(title?.text || "");
+                }}
+              >
+                Update
+              </button>
+
+              <button onClick={() => deleteTitle(title.id)}>Delete</button>
+            </div>
+          </li>
+        );
+      }
+    });
+    setTitles(update);
+  };
+  //<button onClick={() => caseFnCrud(ADD)}>ADD</button>
   return (
     <div className="header">
       <h1>My ToDoList</h1>
+      <div className="todo-add">
+        <input
+          className="todo-add_value"
+          type="text"
+          value={titleSearch}
+          name="titleSearchTodo"
+          onChange={(e) => setTitleSearch(e.target.value)}
+        />
+        <button onClick={() => searchTitle()}>Search</button>
+      </div>
       <p>New Todo</p>
       <div className="todo-add">
         <input
           className="todo-add_value"
           type="text"
           value={title}
+          name="textTodo"
           onChange={(e) => setTitle(e.target.value)}
         />
-        <button onClick={AddTitle}>ADD</button>
+        <button onClick={() => addTitle()}>ADD</button>
       </div>
       <div className="todo-list">
         <p>List Todo</p>
@@ -53,25 +143,36 @@ function TodoList() {
               <li key={titles.id}>
                 <span>{index + 1}</span>
 
-                {todotitleUpdate == titles.id ? (
+                {todoTitleUpdate === titles.id ? (
                   <div>
                     <input
                       type="text"
+                      name="updateTextTodo"
                       value={titleUpdate}
                       onChange={(e) => setTitleUpdate(e.target.value)}
                     />
-                    <button onClick={() => UpdateTitle(titles.id)}>Done</button>
+                    <button onClick={() => updateTitle(titles.id)}>Done</button>
                   </div>
                 ) : (
                   <p>{titles.text}</p>
                 )}
 
                 <div>
-                  <button onClick={() => settodoTitleUpdate(titles.id)}>
+                  <button
+                    onClick={() => {
+                      console.log("titles", titles);
+                      setTodoTitleUpdate(titles.id);
+                      // const tt = titles?.find(
+                      //   (title) => title?.id === titles?.id
+                      // );
+                      // console.log("value", tt);
+                      setTitleUpdate(titles?.text || "");
+                    }}
+                  >
                     Update
                   </button>
 
-                  <button onClick={() => DeleteTitle(titles.id)}>Delete</button>
+                  <button onClick={() => deleteTitle(titles.id)}>Delete</button>
                 </div>
               </li>
             );
@@ -80,6 +181,6 @@ function TodoList() {
       </div>
     </div>
   );
-}
+};
 
 export default TodoList;
